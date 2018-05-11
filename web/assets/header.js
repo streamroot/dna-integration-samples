4b2671d68448db1dd1523761fe09e08c07747d1f
emilepommier <emile.pommieru@streamroot.io>
2018-05-11 19:24:29 +0200

    Update all web players + font

--- web/assets/header.js
+++ web/assets/header.js
@@ -4,6 +4,5 @@ window.addEventListener('load', function()
 
 	header.innerHTML= "<ul><li><img src='${window.location.origin}/../../assets/img/logo_typo_long.svg' height=30></li>"+
 						"<li>" + document.title +"</li>"+
-						"<li><a href='#'' target='_blank'>New user</a></li>"+
-						"<li><a href='${window.location.origin}/../../index.html'>Back</a></li>";
+						"<li><a href='../index.html'>BACK</a></li>";
 });
