fe1ed75b215b5490892e36c59196b194062c4813
Jean-Baptiste Louazel <jb.louazel@gmail.com>
2018-05-22 16:12:27 +0200

    Add exoplayer sample app with Android SDK 3.0.0
    Signed-off-by: Jean-Baptiste Louazel <jb.louazel@gmail.com>

---
+++ android/ExoPlayer/app/src/main/java/io/streamroot/dna/exoplayer/ExoPlayerInteractor.kt
@@ -0,0 +1,44 @@
+package io.streamroot.dna.exoplayer
+
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.Timeline
+import io.streamroot.dna.core.PlayerInteractor
+import io.streamroot.dna.core.TimeRange
+
+class ExoPlayerInteractor(
+        private val player: SimpleExoPlayer
+) : PlayerInteractor {
+    override fun loadedTimeRanges(): MutableList<TimeRange> {
+        val shift = getCurrentWindowShift()
+        val rangeDurationMs = player.bufferedPosition - player.currentPosition
+        val timeRanges: MutableList<TimeRange> = ArrayList()
+
+        if (rangeDurationMs > 0) {
+            timeRanges.add(
+                    TimeRange(
+                            shift + player.currentPosition,
+                            rangeDurationMs
+                    )
+            )
+        }
+
+        return timeRanges
+    }
+
+    override fun playbackTime(): Long {
+        return getCurrentWindowShift() + player.currentPosition
+    }
+
+    private fun getCurrentWindowShift(): Long {
+        val current = player.currentTimeline
+        val timelineWindow = Timeline.Window()
+        var shift: Long = 0
+
+        if (player.currentWindowIndex < current?.windowCount!!) {
+            player.currentTimeline?.getWindow(player.currentWindowIndex, timelineWindow)
+            shift = timelineWindow.positionInFirstPeriodMs
+        }
+
+        return shift
+    }
+}
---
+++ android/ExoPlayer/app/src/main/java/io/streamroot/dna/exoplayer/MainActivity.kt
@@ -0,0 +1,28 @@
+package io.streamroot.dna.exoplayer
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.AutoCompleteTextView
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var streamEditText: AutoCompleteTextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        streamEditText = findViewById(R.id.streamEditText)
+
+        findViewById<Button>(R.id.launchButton).setOnClickListener {
+            val intent = Intent(applicationContext, PlayerActivity::class.java)
+
+            intent.putExtra("streamUrl", streamEditText.text.toString())
+            intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+            applicationContext.startActivity(intent)
+        }
+    }
+}
