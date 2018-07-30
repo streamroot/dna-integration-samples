629a19e6f07058a8120074d70e3999dd84ff9ef7
emilepommier <emile.pommieru@streamroot.io>
2018-07-30 11:36:42 +0200

    Add Akamai MP demo page

--- web/assets/header.js
+++ web/assets/header.js
@@ -1,15 +1,8 @@
-window.addEventListener('load', function () {
+window.addEventListener('load', function()
+{
 	var header = document.getElementsByTagName("header")[0];
 
-	if (location.pathname.substring(4) == 'akamai.html') {
-		header.innerHTML = "<ul><li><img src='${window.location.origin}/../../assets/img/logo_typo_long.svg' height=30></li>" +
-			"<li>" + document.title + "</li>" +
-			"<li><a href='../../index.html'>BACK</a></li>";
-	} else {
-		header.innerHTML = "<ul><li><img src='${window.location.origin}/../../assets/img/logo_typo_long.svg' height=30></li>" +
-			"<li>" + document.title + "</li>" +
-			"<li><a href='../index.html'>BACK</a></li>";
-	}
-
-
+	header.innerHTML= "<ul><li><img src='${window.location.origin}/../../assets/img/logo_typo_long.svg' height=30></li>"+
+						"<li>" + document.title +"</li>"+
+						"<li><a href='../index.html'>BACK</a></li>";
 });
