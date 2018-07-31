e5d2b155f5e6bea269fcc6a22b62fef4a68083af
emilepommier <emile.pommieru@streamroot.io>
2018-07-31 14:10:54 +0200

    Added Kaltura & Mpx demo page

--- web/assets/header.js
+++ web/assets/header.js
@@ -1,8 +1,16 @@
-window.addEventListener('load', function()
-{
+window.addEventListener('load', function () {
+	var player = location.pathname.split('/').slice(-1)[0];
 	var header = document.getElementsByTagName("header")[0];
 
-	header.innerHTML= "<ul><li><img src='${window.location.origin}/../../assets/img/logo_typo_long.svg' height=30></li>"+
-						"<li>" + document.title +"</li>"+
-						"<li><a href='../index.html'>BACK</a></li>";
+	if (player == 'mpx.html' || player == 'kaltura.html') {
+		header.innerHTML = "<ul><li><img src='${window.location.origin}/../../assets/img/logo_typo_long.svg' height=30></li>" +
+			"<li>" + document.title + "</li>" +
+			"<li><a href='#'' target='_blank'>SIMULATE A USER</a></li>" +
+			"<li><a href='../index.html'>BACK</a></li>";
+	} else {
+		header.innerHTML = "<ul><li><img src='${window.location.origin}/../../assets/img/logo_typo_long.svg' height=30></li>" +
+			"<li>" + document.title + "</li>" +
+			"<li><a href='../index.html'>BACK</a></li>";
+	}
+
 });
