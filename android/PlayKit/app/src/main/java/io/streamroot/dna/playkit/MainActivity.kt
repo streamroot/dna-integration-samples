e8b1b96b6d5e402e08a4b8d4258bcbe0b96a2db2
Tri NGUYEN <tri170391@gmail.com>
2019-01-31 18:56:04 +0100

    Working UI mock

--- android/PlayKit/app/src/main/java/io/streamroot/dna/playkit/MainActivity.kt
+++ android/PlayKit/app/src/main/java/io/streamroot/dna/playkit/MainActivity.kt
@@ -1,12 +1,22 @@
 package io.streamroot.dna.playkit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.launchButton).setOnClickListener(View.OnClickListener {
+            val intent = Intent(applicationContext, PlayerActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            applicationContext.startActivity(intent)
+        })
     }
 }
---
+++ android/PlayKit/app/src/main/java/io/streamroot/dna/playkit/PlayerActivity.kt
@@ -0,0 +1,12 @@
+package io.streamroot.dna.playkit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+    }
+}
