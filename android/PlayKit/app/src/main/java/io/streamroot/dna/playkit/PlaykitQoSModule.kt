22ba2c35739ceab0a2f0f9c61b0331aefac07a0c
Tri NGUYEN <tri170391@gmail.com>
2019-02-18 14:30:18 +0100

    Fix QoSModule mis-implemeted

--- android/PlayKit/app/src/main/java/io/streamroot/dna/playkit/PlaykitQoSModule.kt
+++ android/PlayKit/app/src/main/java/io/streamroot/dna/playkit/PlaykitQoSModule.kt
@@ -29,6 +29,10 @@ class PlayKitQoSModule(
             mLastAudioBitrate = playbackInfo.audioBitrate
         }), PlayerEvent.Type.PLAYBACK_INFO_UPDATED)
 
+        mPlayer.addEventListener((PKEvent.Listener<PlayerEvent> {
+            playbackStateChange(PlaybackState.ENDED)
+        }), PlayerEvent.Type.ENDED)
+
         mPlayer.addStateChangeListener {
             val eventDetails = it as PlayerEvent.StateChanged
 
@@ -41,8 +45,8 @@ class PlayKitQoSModule(
                 }
                 PlayerState.READY -> {
                     when (mPlayer.isPlaying) {
-                        true -> PlaybackState.PLAYING
-                        false -> PlaybackState.PAUSING
+                        true -> playbackStateChange(PlaybackState.PLAYING)
+                        false -> playbackStateChange(PlaybackState.PAUSING)
                     }
                 }
             }
