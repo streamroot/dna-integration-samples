78970353ebb4cd81c2d4e8e92090248699b0a27c
LamineNdy <lamine.ndiaye@streamroot.io>
2019-08-13 18:45:31 +0200

    Dna 2.16 (#26)
    * Update brightcove project to android UX
    
    * Update iOS DNA samples
    
    * Update iOS DNA samples

--- android/Brightcove/app/src/main/java/io/streamroot/dna/samples/brightcove/MainActivity.java
+++ android/Brightcove/app/src/main/java/io/streamroot/dna/samples/brightcove/MainActivity.java
@@ -2,7 +2,8 @@ package io.streamroot.dna.samples.brightcove;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.widget.Button;
 import android.widget.EditText;
 
