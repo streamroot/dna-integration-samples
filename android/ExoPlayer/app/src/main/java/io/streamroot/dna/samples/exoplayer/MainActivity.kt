8b1f84271562b5f91a85da95a482fb9073c2c966
Boris Borgobello <boris.borgobello@streamroot.io>
2020-01-10 18:07:20 +0100

    Added Youbora to Exo Sample

--- android/ExoPlayer/app/src/main/java/io/streamroot/dna/samples/exoplayer/MainActivity.kt
+++ android/ExoPlayer/app/src/main/java/io/streamroot/dna/samples/exoplayer/MainActivity.kt
@@ -2,27 +2,27 @@ package io.streamroot.dna.samples.exoplayer
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.AutoCompleteTextView
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var streamEditText: AutoCompleteTextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        streamEditText = findViewById(R.id.streamEditText)
-
-        findViewById<Button>(R.id.launchButton).setOnClickListener {
-            val intent = Intent(applicationContext, PlayerActivity::class.java)
-
-            intent.putExtra("streamUrl", streamEditText.text.toString())
-            intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-
-            applicationContext.startActivity(intent)
+        launchButton.setOnClickListener {
+            PlayerActivity.makeIntent(this,
+                PlayerActivity.PlayerActivityArgs(
+                    streamEditText.text.toString(),
+                    youboraEditText.text.toString()
+                )
+            ).apply {
+                addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            }.run {
+                startActivity(this)
+            }
         }
     }
 }
