2c76e2e4a0d968ad915df6ddce5467bde8efc258
Lamine Ndiaye <lamine.ndy@gmail.com>
2020-01-28 16:10:59 +0100

    Use the AVplayer plugin

--- ios/TVML/TVMLSample/Streamroot/DNAPlayerViewController.swift
+++ ios/TVML/TVMLSample/Streamroot/DNAPlayerViewController.swift
@@ -10,16 +10,17 @@ import Foundation
 
 import AVFoundation
 import AVKit
-import StreamrootSDK
+import AVPlayerDNAPlugin
 
 class DNAPlayerViewController: AVPlayerViewController {
-  var dnaClient: DNAClient?
+  var strPlugin: StreamrootPlugin?
   var displayStatView: Bool = false
   var contentId: String?
   var manifestUrlString: String?
+  var latency = 30
   
   override func viewDidDisappear(_ animated: Bool) {
-    dnaClient?.stop()
+    strPlugin?.stop()
   }
   
   override func viewDidAppear(_ animated: Bool) {
@@ -31,76 +32,21 @@ class DNAPlayerViewController: AVPlayerViewController {
         return
     }
     
-    var builder = DNAClient.builder().dnaClientDelegate(self)
+    let config = DNAConfig(/*streamrootKey: "demoswebsiteandpartners"*/)
+    // the streamroot key will default to the one in the Info.plist if not overridden here
     
+    // set the latency
+    config.latency = latency
+    strPlugin = AVPlayerDNAPlugin(manifestUrl: manifestURL, config: config)
     do {
-      if let contentId = self.contentId {
-        builder = try builder.contentId(contentId)
+      if let localPath = try strPlugin?.start() {
+        player = AVPlayer(url: localPath)
+        try strPlugin?.linkPlayer(player)
+        player?.play()
+        strPlugin?.dnaClient?.displayStats(onView: contentOverlayView!)
       }
-      dnaClient = try builder
-        // the streamroot key will default to the one in the Info.plist if not overridden here
-        //.streamrootKey("demoswebsiteandpartners")
-        .latency(30)
-        .start(manifestURL)
     } catch let error {
-      print("\(error)")
-    }
-    
-    guard let localPath = self.dnaClient?.manifestLocalURLPath,
-      let localUrl = URL(string: localPath) else {
-        print("Could not generate localPath, please check your network")
-        return
-    }
-    
-    let playerItem = AVPlayerItem(asset: AVURLAsset(url: localUrl))
-    player = AVPlayer(playerItem: playerItem)
-    player?.play()
-    view.setNeedsLayout()
-    view.layoutIfNeeded()
-    if displayStatView {
-      dnaClient?.displayStats(onView: contentOverlayView!)
-    }
-  }
-}
-
-//MARK: - DNAClientDelegate
-
-extension DNAPlayerViewController: DNAClientDelegate {
-  func playbackTime() -> Double {
-    if let player = self.player {
-      return CMTimeGetSeconds(player.currentTime())
-    }
-    return 0
-  }
-  
-  func loadedTimeRanges() -> [NSValue] {
-    guard let player = self.player else {
-      return []
-    }
-    
-    guard let playerItem = player.currentItem else {
-      return []
-    }
-    
-    return playerItem.loadedTimeRanges.map { (value) -> NSValue in
-      NSValue(timeRange: TimeRange(range: value.timeRangeValue))
-    }
-  }
-  
-  func updatePeakBitRate(_ bitRate: Double) {
-    player?.currentItem?.preferredPeakBitRate = bitRate
-  }
-  
-  public func bufferTarget() -> Double {
-    if #available(iOS 10.0, tvOS 10.0, *) {
-      return self.player?.currentItem?.preferredForwardBufferDuration ?? 0
-    }
-    return 0.0
-  }
-  
-  public func setBufferTarget(_ target: Double) {
-    if #available(iOS 10.0, tvOS 10.0, *) {
-      self.player?.currentItem?.preferredForwardBufferDuration = target
+      print(error.localizedDescription)
     }
   }
 }
