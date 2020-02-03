c4ccf9a29becf10db524f333980cfa59a066c9d1
emilepommier <36151637+emilepommier@users.noreply.github.com>
2020-02-03 14:30:29 +0100

    Fix header script

--- web/assets/header.js
+++ web/assets/header.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function () {
 	var player = location.pathname.split('/').slice(-1)[0];
 	var header = document.getElementsByTagName("header")[0];
 
-	if (player == 'mpx.html' || player == 'kaltura.html') {
+	if (player == 'mpx.html') {
 		header.innerHTML = "<ul><li><img src='//samples.streamroot.io/web/assets/img/logo_typo_long.svg' height=30></li>" +
 			"<li>" + document.title + "</li>" +
 			"<li><a href='#'' target='_blank'>SIMULATE A USER</a></li>" +
